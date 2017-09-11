770d97ad2b6a5a6f84af7bb3030fd871203aa85d
BugleJones <spencerjhamilton@gmail.com>
2017-09-11 23:49:57 +0000

    Revised download_avatars.js to print out a concatenated string

--- download_avatars.js
+++ download_avatars.js
@@ -2,12 +2,17 @@ var request = require("request");
 
 console.log("Welcome to the Github Avatar Downloader");
 
-
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+  var requestURL = "https://" + GITHUB_USER + ":" + GITHUB_TOKEN + "@api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
+  console.log(requestURL);
 }
+
+var GITHUB_USER = "YOUR USERNAME HERE";
+var GITHUB_TOKEN = "YOUR ACCESSTOKEN HERE";
 
 getRepoContributors("nodejs", "node", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
 });
+
+getRepoContributors();
