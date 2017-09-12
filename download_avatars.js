eddee45f0c30f9cda4fe7216abe8432ed0550937
BugleJones <spencerjhamilton@gmail.com>
2017-09-12 01:10:11 +0000

    Made changes to display the body

--- download_avatars.js
+++ download_avatars.js
@@ -1,18 +1,49 @@
+require('dotenv').config();
+
 var request = require("request");
+var GITHUB_USER = "BugleJones";
+var GITHUB_TOKEN = "dc225ff9ca7e47c3ac9c16175d3d13c6b0fdd733";
 
 console.log("Welcome to the Github Avatar Downloader");
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  var requestURL = "https://" + GITHUB_USER + ":" + GITHUB_TOKEN + "@api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
-  console.log(requestURL);
+  var requestURL = "https://" + GITHUB_USER + ":" + GITHUB_TOKEN + "@api.github.com/repos/"
+  + repoOwner + "/" + repoName + "/contributors";
+  var options = {
+    url: requestURL,
+    headers: {
+    "User-Agent": "BugleJones"
+  }
+};
+
+  request.get(options, function(error, response, body) {
+    var data = JSON.parse(body)
+    console.log(data);
+  });
 }
 
-var GITHUB_USER = "YOUR USERNAME HERE";
-var GITHUB_TOKEN = "YOUR ACCESSTOKEN HERE";
 
-getRepoContributors("nodejs", "node", function(err, result) {
-  console.log("Errors:", err);
+getRepoContributors("jquery", "jquery", function(err, result, body) {
+  if (error) {
+    console.log("Errors: " + err);
+    return;
+  }
   console.log("Result:", result);
 });
 
-getRepoContributors();
+// console.log(options);
+//
+// getRepoContributors("jquery", "jquery", function(err, result, body) {
+//   if (error) {
+//     console.log("Errors: " + err);
+//     return;
+//   }
+//   const data = JSON.parse(body);
+//   data.ForEach((repo) => {
+//     console.log(repo.name, repo.avatar_url);
+//   });
+//   console.log("Result:", result);
+//   console.log("Body:", body);
+//  });
+//
+// }
